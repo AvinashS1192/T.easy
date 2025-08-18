aac3e1c1f33967ba843c646cf54e09291af4f93b
Avinash Singh <avinashbsingh1192@gmail.com>
2025-08-18 23:46:34 +0530

    add background hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Navbar } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      <Hero />
     </div>
   );
 };
